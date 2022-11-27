c0731f3e5a15e7dc5723a84cc17dde89521ee494
Zigmārs Strods <zigmars.strods@gmail.com>
2022-11-27 19:24:43 +0200

    Small optmization with Lombok in SynonymsController

--- src/main/java/io/codelex/synonymsapp/synonyms/SynonymsController.java
+++ src/main/java/io/codelex/synonymsapp/synonyms/SynonymsController.java
@@ -1,5 +1,6 @@
 package io.codelex.synonymsapp.synonyms;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Set;
 
 @RestController
+@AllArgsConstructor
 public class SynonymsController {
 
     private final SynonymsService synonymsService;
-
-    public SynonymsController(SynonymsService synonymsService) {
-        this.synonymsService = synonymsService;
-    }
 
     @GetMapping("/synonyms/{word}")
     public Set<String> getAllSynonyms(@PathVariable String word) {
